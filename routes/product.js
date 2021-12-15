b811ea7d66c7e606de29d1460fd156795914695d
D13G05 <mydiego-99@hotmail.com>
2021-12-15 18:07:25 -0500

    Metodos de isExpired y calcIva añadidos

--- routes/product.js
+++ routes/product.js
@@ -10,11 +10,11 @@ const {
 const router = Router();
 
 router.get('/', getProducts);
-//router.get('/:id', getProduct);
+router.get('/:id', getProduct);
 router.post('/', newProduct);
 router.put('/:id', updateProduct);
 router.delete('/:id', deleteProduct);
-//router.get('/:id', isExpired);
-router.get('/:id', calcIva);
+router.get('/expired/:id', isExpired);
+router.get('/iva/:id', calcIva);
 
 module.exports = router;
