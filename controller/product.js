529e6c87fac3eedb6eaf9ee6766ddb7bc73bbb51
D13G05 <mydiego-99@hotmail.com>
2021-12-15 16:06:58 -0500

    Modificacion clases prodcut

--- controller/product.js
+++ controller/product.js
@@ -1,18 +1,69 @@
 const Product = require('../models/Product');
 
 const getProducts = async (req, res) => {
-    res.send({ product: "Productos" });
+    //res.send({ product: "Productos" });
+    try {
+        const product = await Product.find();
+        return res.status(200).json({
+            message: "Success",
+            data: product
+        });
+    } catch (error) {
+        return res.status(500).json({
+            message: "Error",
+            error: error.message
+        });
+    }
 };
 
 const getProduct = async (req, res) => {
+    try {
+        const product = await Product.find();
+        return res.status(200).json({
+            message: "Success",
+            data: product
+        });
+    } catch (error) {
+        return res.status(500).json({
+            message: "Error",
+            error: error.message
+        });
+    }
 
 };
 
 const newProduct = async (req, res) => {
+    try {
+        const product = new Product(req.body);
+        await product.save();
+        return res.status(200).json({
+            message: "Success",
+            product
+        })
+    } catch (error) {
+        return res.status(500).json({
+            message: "Error",
+            error: error.message
+        });
+    }
 
 };
 
 const updateProduct = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const product = req.body;
+        const result = await Product.findByIdAndUpdate(id, product);
+        return res.status(200).json({
+            message: "Success",
+            data: result
+        })
+    } catch (error) {
+        return res.status(500).json({
+            message: "Error",
+            error: error.message
+        });
+    }
 
 };
 
@@ -30,6 +81,14 @@ const deleteProduct = async (req, res) => {
             error: error.message
         });
     }
+};
+
+const isExpired = async (req, res) => {
+    
+};
+
+const calcIva = async (req, res) => {
+
 };
 
 module.exports = {
